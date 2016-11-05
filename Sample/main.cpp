45d5c014d697274a6444f3fc2e36f069e1a52f0a
大石头 <nnhy@vip.qq.com>
2016-11-05 16:46:29 +0800

    GCC编译RTL8710AF成功通过！此计划结束！

--- Sample/main.cpp
+++ Sample/main.cpp
@@ -3,7 +3,6 @@ extern "C" {
 #include "task.h"
 #include "diag.h"
 #include "main.h"
-#include <example_entry.h>
 
 extern int rtl_cryptoEngine_init(void);
 extern void console_init(void);
@@ -20,19 +19,13 @@ int main(void)
 	console_init();
 
 	DiagPrintf("万家灯火，无声物联！");
-	
-	/* pre-processor of application example */
-	pre_example_entry();
 
 	/* wlan intialization */
 #if defined(CONFIG_WIFI_NORMAL) && defined(CONFIG_NETWORK)
 	wlan_network();
 #endif
 
-	/* Execute application example */
-	example_entry();
-
-    	/*Enable Schedule, Start Kernel*/
+    /*Enable Schedule, Start Kernel*/
 #if defined(CONFIG_KERNEL) && !TASK_SCHEDULER_DISABLED
 	#ifdef PLATFORM_FREERTOS
 	vTaskStartScheduler();
