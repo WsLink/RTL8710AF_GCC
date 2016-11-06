c3e3b7be2e73f3c90f35880afad2e878ac4417dc
大石头 <nnhy@vip.qq.com>
2016-11-07 01:33:16 +0800

    引用这个变量可导致 lib_platform.a(startup.o) 被链接，从而确保整体成功

--- Sample/main.cpp
+++ Sample/main.cpp
@@ -1,6 +1,12 @@
 extern "C" {
 	#include "diag.h"
-	#include "gpio_api.h"   // mbed
+	#include "gpio_api.h"
+
+	// 引用这个变量可导致 lib_platform.a(startup.o) 被链接，从而确保整体成功
+	#if !defined (__ICCARM__)
+		extern u8 RAM_IMG1_VALID_PATTEN[];
+		void *tmp = RAM_IMG1_VALID_PATTEN;
+	#endif
 }
 
 int main(void)
@@ -9,10 +15,9 @@ int main(void)
 
 	DiagPrintf("万家灯火，无声物联！\r\n");
 
-    // Init LED control pin
     gpio_init(&gpio_led, PC_5);
-    gpio_dir(&gpio_led, PIN_OUTPUT);    // Direction: Output
-    gpio_mode(&gpio_led, PullNone);     // No pull
+    gpio_dir(&gpio_led, PIN_OUTPUT);
+    gpio_mode(&gpio_led, PullNone);
     while(1){
         gpio_write(&gpio_led, !gpio_read(&gpio_led));
         //RtlMsleepOS(1000);
